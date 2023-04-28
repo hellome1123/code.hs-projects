0b730aea8dad85556bd02fe50acd1095b3092ee7
hellome1123 <117306771+hellome1123@users.noreply.github.com>
2023-04-28 11:03:47 -0700

    Update new game code .py

--- new game code .py
+++ new game code .py
@@ -1,5 +1,23 @@
 from random import randint
 from time import sleep
+import time
+import sys
+
+username = input("Enter username:")
+print("Username is: " + username)
+
+#animation = "|/-\\"
+
+#animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+
+for i in range(20):
+    time.sleep(0.1)
+    sys.stdout.write("\rwaiting.... " + animation[i % len(animation)])
+    sys.stdout.flush()
+    
+print("\nDone!")
 
 while True:
   a = randint(1000000000000000000000000000000000000000000000000, 9999999999999999999999999999999999999999999999999)
