05229df6e9ac15bc684cf20604dc50d37a34435e
hellome1123 <117306771+hellome1123@users.noreply.github.com>
2023-04-28 11:04:35 -0700

    Update new game code .py

--- new game code .py
+++ new game code .py
@@ -23,12 +23,12 @@ while True:
   a = randint(1000000000000000000000000000000000000000000000000, 9999999999999999999999999999999999999999999999999)
   b = randint(0, 75)
   if b == 0:
-    print('\033[91m' + 'ERROR: Virus detected')
-    print('\033[91m' + 'ERROR: Could not open AVG Antivirus: insufficient permissions')
+    print('\033[91m' + 'ERROR: problrm detected')
+    print('\033[91m' + 'ERROR: thbc not working')
     sleep(1.5)
   if b ==  1:
     c = randint(300, 900)
-    print('\033[93m' + 'Deleted ' + str(c) + ' files')
+    print('\033[93m' + 'fixing ' + str(c) + ' files')
     sleep(0.75)
   else:
     print('\033[92m' + str(a))
